54067301b9ab9d9046e6085602959229a4dbc862
Ana Lopez <anai_lopezh@hotmail.com>
2022-09-13 21:20:31 -0500

    tareas1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Componentes/Header'
+
+function App() {
+  
+
+  return (
+    <div className='container'>
+     <Header titulo='Tareas' />
+    </div>
+  )
+}
+
+export default App
+
+
